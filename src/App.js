e46418a2962072d178539afb4a9c0921a1dab5b4
moitad12 <106792020+moitad12@users.noreply.github.com>
2022-12-13 20:03:38 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
+function App(){
+    let colors = [
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ]
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock key={i} color={color}/>
+        )
+    })
+
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor} />
+        </div>
+    )
+
+}
+
+export default App;
